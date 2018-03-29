81530e4ba8123aa53c552864aff5a2ba9e008178
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-03-29 15:05:25 +0200

    FEATURE: change keypress event listener to 'keydown' for sharper controls

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
